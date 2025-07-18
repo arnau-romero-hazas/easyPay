d656ce4069fcf85a95bc0e1451605b2579e8f2c1
arnau-romero-hazas <aromerohazas@gmail.com>
2025-07-18 12:58:58 +0200

    Add User domain model with validation

---
+++ backend/src/domain/User.js
@@ -0,0 +1,16 @@
+// src/domain/User.js
+
+class User {
+  constructor({ id = null, name, email, passwordHash }) {
+    if (!name || !email || !passwordHash) {
+      throw new Error('Missing required fields to create a User')
+    }
+
+    this.id = id
+    this.name = name
+    this.email = email
+    this.passwordHash = passwordHash
+  }
+}
+
+module.exports = User
